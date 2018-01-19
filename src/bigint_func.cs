8e1b7767f115218e62f336f54f4a1e2b343632f4
McTwist <rajb89@hotmail.com>
2018-01-19 22:04:32 +0100

    Added small number optimizations

--- src/bigint_func.cs
+++ src/bigint_func.cs
@@ -12,6 +12,17 @@
 // Add two values together and return their sum
 function bigint_add(%a, %b)
 {
+	%a_l = strlen(%a);
+	%b_l = strlen(%b);
+	if (%a_l < 6 || %b_l < 6)
+	{
+		return %a + %b;
+	}
+	else if (%a_l < 8 && %b_l < 8)
+	{
+		return uint_add(%a, %b);
+	}
+
 	%a = bigint__parseValue(%a);
 	%b = bigint__parseValue(%b);
 	if (%a.sign != %b.sign)
@@ -35,6 +46,17 @@ function bigint_add(%a, %b)
 // Subtract two values and retiurn their difference
 function bigint_subtract(%a, %b)
 {
+	%a_l = strlen(%a);
+	%b_l = strlen(%b);
+	if (%a_l < 6 && %b_l < 6)
+	{
+		return %a - %b;
+	}
+	else if (%a_l < 8 && %b_l < 8)
+	{
+		return uint_sub(%a, %b);
+	}
+
 	%a = bigint__parseValue(%a);
 	%b = bigint__parseValue(%b);
 	if (%a.sign != %b.sign)
@@ -58,6 +80,17 @@ function bigint_subtract(%a, %b)
 // Multiply two values together and return their product
 function bigint_multiply(%a, %b)
 {
+	%a_l = strlen(%a);
+	%b_l = strlen(%b);
+	if (%a_l < 4 && %b_l < 4)
+	{
+		return %a * %b;
+	}
+	else if (%a_l < 5 && %b_l < 5)
+	{
+		return uint_mul(%a, %b);
+	}
+
 	%a = bigint__parseValue(%a);
 	%b = bigint__parseValue(%b);
 	%sign = %a.sign != %b.sign;
@@ -81,6 +114,20 @@ function bigint_multiply(%a, %b)
 // Get the remainder from $bigint::remainder
 function bigint_divmod(%a, %b)
 {
+	%a_l = strlen(%a);
+	%b_l = strlen(%b);
+	if (%a_l < 6 && %b_l < 6)
+	{
+		$bigint::remainder = %a % %b;
+		return mFloor(%a / %b);
+	}
+	else if (%a_l < 8 && %b_l < 8)
+	{
+		%ret = uint_div(%a, %b);
+		$bigint::remainder = $uint::remainder;
+		return %ret;
+	}
+
 	%a = bigint__parseValue(%a);
 	%b = bigint__parseValue(%b);
 	%sign = %a.sign != %b.sign;
