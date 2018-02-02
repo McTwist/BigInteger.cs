47676f415470444964f79231d38837243d83531d
McTwist <rajb89@hotmail.com>
2018-02-02 14:17:43 +0100

    Removed num2str

--- src/uint.cs
+++ src/uint.cs
@@ -115,23 +115,3 @@ function uint_pow(%a, %b)
 
 	return %r;
 }
-
-// Converts a number to a string correctly
-function num2str(%num)
-{
-	if (mAbs(%num) < 1000000)
-		return "" @ %num;
-	if (%num < 0)
-	{
-		%neg = true;
-		%num = -%num;
-	}
-	%decimals = %num - mFloor(%num);
-	%decilen = strlen(%decimals);
-	%million = uint_div(%num, 1000000);
-	%num = $uint::remainder;
-	%len = 6 - strlen(%num);
-	for (%i = 0; %i < %len; %i++)
-		%num = 0 @ %num;
-	return (%neg ? "-" : "") @ %million @ %num @ (%decilen > 1 ? getSubStr(%decimals, 1, %decilen) : "");
-}
